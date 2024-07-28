e447f71ea9f7fef1f8dd7640904ae2ecb2ef42d9
Rex Aliis <77251557+RexAliis@users.noreply.github.com>
2024-07-28 00:50:33 -0300

    feat: tuple converter
    add a class that inherits JsonConverter to add functionality and a JsonConverterFactory that works with Tuple and ValueTuple

---
+++ TupleJsonConverters/ExtendedJsonConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace TupleJsonConverters
+{
+    internal abstract class ExtendedJsonConverter<TypeToConvert> : JsonConverter<TypeToConvert>
+    {
+        protected static T GetNextValue<T>(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            _ = reader.Read();
+            return options.GetConverter(typeof(T)) is JsonConverter<T> converter
+            ? converter.Read(ref reader, typeToConvert, options)!
+            : JsonSerializer.Deserialize<T>(ref reader, options)!;
+        }
+        protected static void WriteValue<T>(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
+        {
+            if (options.GetConverter(typeof(T)) is JsonConverter<T> converter) converter.Write(writer, value, options);
+            else JsonSerializer.Serialize(writer, value, options);
+        }
+    }
+}
---
+++ TupleJsonConverters/TupleConverter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace TupleJsonConverters
+{
+    public sealed class TupleConverter : JsonConverterFactory
+    {
+        public override bool CanConvert(Type typeToConvert)
+        {
+            if (!typeToConvert.IsGenericType) return false;
+
+            Type typeDefinition = typeToConvert.GetGenericTypeDefinition();
+
+            return typeToConvert.IsClass
+                ? typeDefinition == typeof(Tuple<>)
+                    || typeDefinition == typeof(Tuple<,>)
+                    || typeDefinition == typeof(Tuple<,,>)
+                    || typeDefinition == typeof(Tuple<,,,>)
+                    || typeDefinition == typeof(Tuple<,,,,>)
+                    || typeDefinition == typeof(Tuple<,,,,,>)
+                    || typeDefinition == typeof(Tuple<,,,,,,>)
+                    || typeDefinition == typeof(Tuple<,,,,,,,>)
+                : typeDefinition == typeof(ValueTuple<>)
+                    || typeDefinition == typeof(ValueTuple<,>)
+                    || typeDefinition == typeof(ValueTuple<,,>)
+                    || typeDefinition == typeof(ValueTuple<,,,>)
+                    || typeDefinition == typeof(ValueTuple<,,,,>)
+                    || typeDefinition == typeof(ValueTuple<,,,,,>)
+                    || typeDefinition == typeof(ValueTuple<,,,,,,>)
+                    || typeDefinition == typeof(ValueTuple<,,,,,,,>);
+        }
+        public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+        {
+            Type converter = GetConverterType(typeToConvert);
+            return (Activator.CreateInstance(converter) as JsonConverter)!;
+        }
+        private static Type GetConverterType(Type type)
+        {
+            Type[] arguments = type.GetGenericArguments();
+
+            return type.IsClass
+                ? arguments.Length switch
+                {
+                    1 => typeof(TupleJsonConverter<>).MakeGenericType(arguments),
+                    2 => typeof(TupleJsonConverter<,>).MakeGenericType(arguments),
+                    3 => typeof(TupleJsonConverter<,,>).MakeGenericType(arguments),
+                    4 => typeof(TupleJsonConverter<,,,>).MakeGenericType(arguments),
+                    5 => typeof(TupleJsonConverter<,,,,>).MakeGenericType(arguments),
+                    6 => typeof(TupleJsonConverter<,,,,,>).MakeGenericType(arguments),
+                    7 => typeof(TupleJsonConverter<,,,,,,>).MakeGenericType(arguments),
+                    8 => typeof(TupleJsonConverter<,,,,,,,>).MakeGenericType(arguments),
+                    _ => throw new ArgumentOutOfRangeException()
+                }
+                : arguments.Length switch
+                {
+                    1 => typeof(ValueTupleJsonConverter<>).MakeGenericType(arguments),
+                    2 => typeof(ValueTupleJsonConverter<,>).MakeGenericType(arguments),
+                    3 => typeof(ValueTupleJsonConverter<,,>).MakeGenericType(arguments),
+                    4 => typeof(ValueTupleJsonConverter<,,,>).MakeGenericType(arguments),
+                    5 => typeof(ValueTupleJsonConverter<,,,,>).MakeGenericType(arguments),
+                    6 => typeof(ValueTupleJsonConverter<,,,,,>).MakeGenericType(arguments),
+                    7 => typeof(ValueTupleJsonConverter<,,,,,,>).MakeGenericType(arguments),
+                    8 => typeof(ValueTupleJsonConverter<,,,,,,,>).MakeGenericType(arguments),
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+        }
+    }
+}
