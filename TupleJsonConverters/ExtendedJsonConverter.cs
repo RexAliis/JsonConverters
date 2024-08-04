182080e2f27d00418bb8492614db55c42ad82274
Rex Aliis <rexaliisdev@gmail.com>
2024-08-04 17:50:19 -0300

    Code cleanup and analysis

--- TupleJsonConverters/ExtendedJsonConverter.cs
+++ TupleJsonConverters/ExtendedJsonConverter.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace TupleJsonConverters
 {
-    public abstract class ExtendedJsonConverter<TypeToConvert> : JsonConverter<TypeToConvert>
+    [SuppressMessage("Design", "CA1062: Validate arguments of public methods", Justification = "Instances should be handled only by the JsonSerializer")]
+    public abstract class ExtendedJsonConverter<TTypeToConvert> : JsonConverter<TTypeToConvert>
     {
         protected static T GetNextValue<T>(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
