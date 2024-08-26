7adf7a5cffa5fa2e5b74cd5c1c1f7b90d54b9608
Rex Aliis <rexaliisdev@gmail.com>
2024-08-26 19:46:03 -0300

    feat: add Optional<T>
    Enhance handling of null and undefined values during serialization and deserialization

---
+++ JsonSerialization/Nullability/Optional.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace JsonSerialization.Nullability
+{
+	[SuppressMessage("Naming", "CA1716: Identifiers should not match keywords")]
+	public readonly struct Optional<T> : IEquatable<Optional<T>> where T : notnull
+	{
+		public readonly T? Value { get; private init; } = default;
+		public readonly bool IsNull { get; private init; } = false;
+		public readonly bool IsUndefined { get; private init; } = true;
+		public static readonly Optional<T> Undefined = new();
+
+		public Optional() { }
+		public Optional(T? value)
+		{
+			Value = value;
+			IsNull = value == null;
+			IsUndefined = false;
+		}
+
+		public override bool Equals(object? obj) => obj is Optional<T> other && Equals(other);
+		public bool Equals(Optional<T> other)
+		{
+			return IsUndefined == other.IsUndefined
+			&& IsNull == other.IsNull
+			&& (IsNull || Value!.Equals(other.Value));
+		}
+		public override int GetHashCode(){
+			return IsUndefined
+			? 0
+			: HashCode.Combine(IsUndefined, IsNull ? 0 : Value!.GetHashCode());
+		}
+
+		public static bool operator ==(Optional<T> left, Optional<T> right) => left.Equals(right);
+
+		public static bool operator !=(Optional<T> left, Optional<T> right) => !(left == right);
+	}
+}
