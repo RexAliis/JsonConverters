92a00bf1fba69b31c91d5278d9b89b3a7a77bbbc
Rex Aliis <rexaliisdev@gmail.com>
2024-08-30 19:38:02 -0300

    fix: fix unintended default/undefined value behavior

--- JsonSerialization/Nullability/Optional.cs
+++ JsonSerialization/Nullability/Optional.cs
@@ -15,47 +15,41 @@ namespace JsonSerialization.Nullability
 		/// Gets the underlying value of the current <see cref="Optional{T}"/> instance.
 		/// The value may be <c>null</c> if <typeparamref name="T"/> is a reference or nullable type.
 		/// </summary>
-		public readonly T? Value { get; private init; } = default;
+		public readonly T? Value { get; private init; }
 
 		/// <summary>
 		/// Gets a value indicating whether the current <see cref="Optional{T}"/> instance contains a non-null value.
 		/// If <typeparamref name="T"/> is a non-nullable type, this property will be <c>true</c> as long as the instance is not in its undefined state.
 		/// </summary>
-		public readonly bool HasValue { get; private init; } = false;
+		public readonly bool HasValue => !IsNull;
 
 		/// <summary>
 		/// Gets a value indicating whether the current <see cref="Optional{T}"/> instance contains a null value.
 		/// This property is always <c>false</c> if <typeparamref name="T"/> is a non-nullable value type.
 		/// </summary>
-		public readonly bool IsNull { get; private init; } = false;
+		public readonly bool IsNull => Value == null;
 
 		/// <summary>
 		/// Gets a value indicating whether the current <see cref="Optional{T}"/> instance is in its undefined state.
 		/// </summary>
-		public readonly bool IsUndefined { get; private init; } = true;
+		public readonly bool IsUndefined => this == default;
 
 		/// <summary>
 		/// Represents a undefined state for <see cref="Optional{T}"/>.
 		/// </summary>
-		public static readonly Optional<T> Undefined = new();
+		public static readonly Optional<T> Undefined;
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="Optional{T}"/> in an undefined state.
 		/// </summary>
-		public Optional() { }
+		public Optional() => Value = default;
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="Optional{T}"/> struct with the specified value.
 		/// If the provided value is null, the instance will be marked as null. 
 		/// </summary>
 		/// <param name="value">The value to be assigned to this instance.</param>
-		public Optional(T value)
-		{
-			Value = value;
-			IsNull = value == null;
-			HasValue = !IsNull;
-			IsUndefined = false;
-		}
+		public Optional(T value) => Value = value;
 
 		/// <inheritdoc/>
 		public override bool Equals(object? obj) => obj is Optional<T> other && Equals(other);
@@ -63,8 +57,7 @@ namespace JsonSerialization.Nullability
 		/// <inheritdoc/>
 		public bool Equals(Optional<T> other)
 		{
-			return IsUndefined == other.IsUndefined
-			&& IsNull == other.IsNull
+			return IsNull == other.IsNull
 			&& (IsNull || Value!.Equals(other.Value));
 		}
 
